0ad1f8716dba5c53e1d9a11bd0056afdeb0a1e39
Alban Siffer <31479857+asiffer@users.noreply.github.com>
2024-05-30 10:10:49 +0200

    update hook

--- hooks/inject-version.sh
+++ hooks/inject-version.sh
@@ -2,5 +2,5 @@
 
 VERSION=$(poetry version -s)
 echo '__version__ = "'"${VERSION}"'"' > mkdocs_apicall_plugin/__init__.py
-sed "s/__version__ == \x22[0-9.]*\x22/__version__ == \x22${VERSION}\x22/" tests/test_mkdocs_apicall_plugin.py
+sed -i "s/__version__ == \x22[0-9.]*\x22/__version__ == \x22${VERSION}\x22/" tests/test_mkdocs_apicall_plugin.py
 exit 0
